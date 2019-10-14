7cc423137e1f22c91cdecd36bafa15ae4593be48
Akshay Suresh Bhat <akshaybhat2012@gmail.com>
2019-10-14 23:03:36 +0530

    Raising 404 error.

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from .models import Question
+from django.http import Http404
 
 # Create your views here.
 
@@ -22,8 +23,15 @@ def index(request):
     return render(request, 'polls/index.html', context)   
 # Leave the rest of the views (detail, results, vote) unchanged
 
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
+
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
